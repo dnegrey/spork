9cf12503eb66039b1bb8e53e7a72201c9afa2d4e
dnegrey <dnegrey@gmail.com>
2016-02-20 11:52:16 -0500

    added readConfig

--- R/readConfig.R
+++ R/readConfig.R
@@ -1,5 +1,17 @@
-readConfig <- function(config) {
-    x <- readLines(config)
+#' @title Read a configuration file into a named list
+#' @description \code{readConfig} reads a configuration file of key value pairs 
+#' and stores the result in a named list
+#' @param f file path to the configuration file
+#' @return A named list of character values where the names are the keys from 
+#' the configuration file and the values are the values from the configuration 
+#' file.
+#' @examples 
+#' writeLines(c("COLOR=blue", "ANIMAL=dog"), "test")
+#' readConfig("test")
+#' @seealso \code{\link{readLines}, \link{strsplit}}
+#' @export 
+readConfig <- function(f) {
+    x <- readLines(f)
     x <- strsplit(x, split = "=", fixed = TRUE)
     y <- lapply(x, function(x){x[[1]]})
     z <- lapply(x, function(x){x[[2]]})
