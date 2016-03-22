d38ec089e3cec30d1e5a33205ca57d6afb1cfca1
dnegrey <dnegrey@gmail.com>
2016-03-22 01:09:48 -0400

    confirmed working examples for draftRmdHTML

--- R/draftRmdHTML.R
+++ R/draftRmdHTML.R
@@ -1,4 +1,29 @@
 #' @title copy RmdHTMLdoc template
+#' @description copy RmdHTMLdoc template
+#' @param dir 
+#' @param logoFile 
+#' @param width 
+#' @param height 
+#' @param moreLogoCSS 
+#' @param recursive 
+#' @return A logical value indicating the success (or failure) of copying the 
+#' template files. If \code{dir} does not exist, its creation will be 
+#' attempted. Template files are then copied to \code{dir}. Appropriate 
+#' modifications are then made to template files based on \code{logoFile}, 
+#' \code{width}, \code{height} and \code{moreLogoCSS}.
+#' @seealso \code{\link{file.copy}, \link{readTemplate}, \link{image_uri}}
+#' @examples 
+#' dir.create("test")
+#' download.file("http://cran.us.r-project.org/Rlogo.svg", "test/Rlogo.svg")
+#' draftRmdHTML("test", "test/Rlogo.svg", 148, 115, "margin-top: 20px;")
+#' rmarkdown::render("test/template.Rmd")
+#' dir.create("test2")
+#' download.file(
+#'     "https://www.rstudio.com/wp-content/uploads/2014/06/RStudio-Ball.png",
+#'     "test2/RStudio-Ball.png"
+#' )
+#' draftRmdHTML("test2", "test2/RStudio-Ball.png", 150, 150)
+#' rmarkdown::render("test2/template.Rmd")
 #' @importFrom knitr image_uri
 #' @export 
 draftRmdHTML <- function(dir, logoFile, width, height,
